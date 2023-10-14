d6878af02fb2c356834ed74f892a303684697eac
IA-LINK <jyc.kom@gmail.com>
2023-10-13 22:39:49 -0700

    environ.c file

---
+++ environ.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+
+
+#define MAX_COMMAND_LENGTH 100
+
+/**
+ * printEnvironment - Print the current environment variables
+ *
+ * This function iterates through the environment variables and prints them.
+ */
+void printEnvironment(void)
+{
+	extern char **environ;
+
+	for (char **env = environ; *env != NULL; env++)
+	{
+		printf("%s\n", *env);
+	}
+}
+
+/**
+ * main - Simple shell with 'env' built-in command
+ *
+ * This program provides a simple shell that allows users to run commands
+ * or use the 'env' command to print the current environment.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char input[MAX_COMMAND_LENGTH];
+
+	while (1)
+	{
+		printf("SimpleShell> ");
+		if (fgets(input, sizeof(input), stdin) == NULL)
+		{
+			break;
+		}
+		input[strcspn(input, "\n")] = '\0';
+		if (strlen(input) == 0)
+		{
+			continue;
+		}
+		if (strcmp(input, "env") == 0)
+		{
+			printEnvironment();
+		}
+	}
+	if (1)
+	{
+		pid_t pid = fork();
+
+		if (pid < 0)
+		{
+			perror("Fork failed");
+		}
+		else if (pid == 0)
+		{
+			if (execlp(input, input, NULL) == -1)
+			{
+				perror("Exec failed");
+				exit(1);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+		return (0);
+	}
+}
